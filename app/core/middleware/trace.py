9b851bd6c9902e888051ab9a2f85fc4db617a7b4
libaiyun <libaiyun@vipinfo.com.cn>
2025-03-21 14:03:55 +0800

    chore: update x-process-time

--- app/core/middleware/trace.py
+++ app/core/middleware/trace.py
@@ -19,5 +19,5 @@ async def add_process_time(request: Request, call_next):
     start_time = time.perf_counter()
     response = await call_next(request)
     process_time = time.perf_counter() - start_time
-    response.headers["X-Process-Time"] = str(process_time)
+    response.headers["X-Process-Time"] = str(round(process_time * 1000))
     return response
